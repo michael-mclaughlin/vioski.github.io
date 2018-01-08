910ef80c2688fa9dc1fc9a7cf461dc537b2fcc76
michael-mclaughlin <mclaughlinuiuxdesign@yahoo.com>
2018-01-07 23:08:15 -0600

    added google maps integration with corporate headquarters as main marker. Fixed link issues

--- scripts/gkey.js
+++ scripts/gkey.js
@@ -1,12 +1,23 @@
 
 function initMap() {
   var usaHome = {lat: 34.1341115, lng: -118.00557800000001};
-  var map = new google.maps.Map(document.getElementById('div-rep-locator'), {
-    zoom: 4,
-    center: usaHome
-  });
-  var marker = new google.maps.Marker({
-    position: usaHome,
-    map: map
+  var map = map;
+  var mapProperties = {
+    center: new google.maps.LatLng(usaHome),
+     zoom: 4
+  };
+  map = new google.maps.Map(document.getElementById('div-rep-locator'), mapProperties);
+
+  var markers = data.markers;
+
+  $.each(data.markers, function(key, data){
+    var latLng = new google.maps.LatLng(data.location[0], data.location[1]);
+
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        //icon: icon,
+        title: data.name
+    });
   });
 }
